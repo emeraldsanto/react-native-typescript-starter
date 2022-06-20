802b2d23e23f92927a36bedefb8885833f896e66
emeraldsanto <yanick.belanger@yahoo.com>
2022-06-19 20:27:02 -0400

    install navigation library

--- android/app/src/main/java/com/reactnativetypescriptstarter/MainActivity.java
+++ android/app/src/main/java/com/reactnativetypescriptstarter/MainActivity.java
@@ -1,5 +1,7 @@
 package com.reactnativetypescriptstarter;
 
+import android.os.Bundle;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -13,6 +15,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "ReactNativeTypescriptStarter";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
